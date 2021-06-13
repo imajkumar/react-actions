0fb7be8906e19a6062fcef013942309c4aad7c74
Ajay Ayra <ajayit2020@gmail.com>
2021-06-13 13:12:45 +0530

    added react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Ayra System
-        </p>     
-      
+        <p>Ayra System</p>
       </header>
     </div>
   );
